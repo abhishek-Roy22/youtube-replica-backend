e7f5bfcf03b314b4336c58c829351fa7f03a4be1
abhishek-Roy22 <100830257+abhishek-Roy22@users.noreply.github.com>
2025-01-09 22:07:08 +0530

    resolve some issue

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Server is running BRO!</h1>');
 });
 //To check whether the user is logged
-app.get('/auth/check', authenticateCookie(), (req, res) => {
+app.get('/auth/check', authenticateUser, (req, res) => {
   res.status(200).json({ message: 'User is authenticated', user: req.user });
 });
 app.use('/api/user', userRouter);
